a20a7fe4bd62a7883cf8a875b95c3d8d3dc92d69
umutg <umutg@UMUT>
2021-12-14 12:27:58 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,67 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Car car1 = new Car();
+            car1.CarId = 6;
+            car1.BrandId = 2;
+            car1.ColorId = 3;
+            car1.DailyPrice = 700;
+            car1.Description = "1.6 Dizel";
+
+            Car car2 = new Car();
+            car2.CarId = 7;
+            car2.BrandId = 1;
+            car2.ColorId = 2;
+            car2.DailyPrice = 450;
+            car2.Description = "1.5 Benzin";
+
+            carManager.Add(car1);
+            carManager.Add(car2);
+
+            Console.WriteLine("----------------------------------------");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            carManager.Delete(car2);
+
+            Console.WriteLine("----------------------------------------");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            car1.Description = "1.2 Dizel";
+            carManager.Update(car1);
+
+            Console.WriteLine("----------------------------------------");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            var findCar = carManager.GetById(1);
+            Console.WriteLine(findCar.DailyPrice);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+                new Car{CarId=1,BrandId=2,ColorId=1,DailyPrice=300,Description="1.3 Dizel"},
+                new Car{CarId=2,BrandId=1,ColorId=2,DailyPrice=400,Description="1.4 Benzin"},
+                new Car{CarId=3,BrandId=2,ColorId=1,DailyPrice=300,Description="1.5 Dizel"},
+                new Car{CarId=4,BrandId=1,ColorId=2,DailyPrice=400,Description="1.6 Dizel"},
+                new Car{CarId=5,BrandId=2,ColorId=1,DailyPrice=500,Description="2.0 Benzin"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+            Console.WriteLine(car.CarId+ " Id nolu araç sisteme eklendi");
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+            _cars.Remove(carToDelete);
+            Console.WriteLine(car.CarId + " Id nolu araç sistemden silindi");
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.Find(c => c.CarId == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            
+            Console.WriteLine(carToUpdate.CarId + " Id nolu araç bilgileri güncellendi");
+        }
+    }
+}
